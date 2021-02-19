66411ce08721917737738a7ff3044f9c5756095f
dowk12 <stonerl12@naver.com>
2021-02-20 00:52:47 +0900

    #1. Setting webserver

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+  return "Hello! Welcome to mi casa!"
+
+@app.route("/contact")
+
+def potato():
+  return "Contact me!"
+
+app.run(host="127.0.0.1")
