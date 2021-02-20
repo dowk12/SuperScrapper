67ab8641078589bc5051130f851cbb6277fb59a8
dowk12 <stonerl12@naver.com>
2021-02-20 13:51:37 +0900

    #7. Export Route

--- main.py
+++ main.py
@@ -26,5 +26,21 @@ def report():
 
   return render_template("report.html", SearchingBy=word, resultsNumber=len(jobs), jobs=jobs)
 
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception();
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    return f"Generate CSV for {word}"
+  except:
+    return redirect("/")
+   
+      
+
 
 app.run(host="127.0.0.1")
