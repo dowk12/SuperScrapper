92c244ceb95a27950b59c80990440dead3300318
dowk12 <stonerl12@naver.com>
2021-02-20 12:59:12 +0900

    #5. Faster Scrapper(Fake db)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
 
+db = {}
+
 @app.route("/")
 def home():
   return render_template("potato.html")
@@ -12,12 +14,17 @@ def report():
   word = request.args.get('word')
   if word:
     word = word.lower()
-    jobs = get_jobs(word)
-    print(jobs)
+    fromDb = db.get(word)
+    if fromDb:
+      jobs = fromDb
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
+      print(db[word])
   else:
     return redirect("/")
 
-  return render_template("report.html", SearchingBy=word, potato="sexy")
+  return render_template("report.html", SearchingBy=word, resultsNumber=len(jobs))
 
 
 app.run(host="127.0.0.1")
